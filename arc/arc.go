6d20dc57d6f94e94bcb2e506135ab9f3132d2c29
Gustavo Cavalcante <nowayhecodes@gmail.com>
2022-08-09 23:27:57 -0300

    feat: arc get func

--- arc/arc.go
+++ arc/arc.go
@@ -12,48 +12,80 @@ import (
 // frequency and recency of use, avoiding a burst in access to new
 // entries from evicting the frequently used older entries.
 type ARCCache struct {
-	size int
-	pref int // pref is the dynamic preference towards T1 or T2
 	lock sync.RWMutex
 
-	T1         polacache.LRUCache
-	evictForT1 polacache.LRUCache
+	// Total capacity of the cache
+	size int
 
-	T2         polacache.LRUCache
-	evictForT2 polacache.LRUCache
+	// pref is the dynamic preference towards "recently" or "frequently"
+	pref int
+
+	// LRU for recently accessed items
+	recently polacache.LRUCache
+
+	// LRU evictions for recently accessed items
+	recentlyEviction polacache.LRUCache
+
+	// LRU for frequently accessed items
+	frequently polacache.LRUCache
+
+	// LRU evictions for frequently accessed items
+	frequentlyEviction polacache.LRUCache
 }
 
 // Creates an ARC of given size
 func NewARC(size int) (*ARCCache, error) {
 
-	evictForT1, err := lru.NewLRU(size, nil)
+	recentEvict, err := lru.NewLRU(size, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	evictForT2, err := lru.NewLRU(size, nil)
+	frequentEvict, err := lru.NewLRU(size, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	t1, err := lru.NewLRU(size, nil)
+	recent, err := lru.NewLRU(size, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	t2, err := lru.NewLRU(size, nil)
+	frequent, err := lru.NewLRU(size, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	arc := &ARCCache{
-		size:       size,
-		pref:       0,
-		T1:         t1,
-		evictForT1: evictForT1,
-		T2:         t2,
-		evictForT2: evictForT2,
+		size:               size,
+		pref:               0,
+		recently:           recent,
+		recentlyEviction:   recentEvict,
+		frequently:         frequent,
+		frequentlyEviction: frequentEvict,
 	}
 
 	return arc, nil
+}
+
+// Gets the given value from the cache
+func (arc *ARCCache) Get(key interface{}) (value interface{}, ok bool) {
+	arc.lock.Lock()
+	defer arc.lock.Unlock()
+
+	// Promotes a value to frequently used LRU,
+	// if the recently used LRU contains the given value
+	if val, ok := arc.recently.Peek(key); ok {
+		arc.recently.Remove(key)
+		arc.frequently.Add(key, val)
+
+		return val, ok
+	}
+
+	// Checks if frequently used LRU contains the given value
+	if val, ok := arc.frequently.Get(key); ok {
+		return val, ok
+	}
+
+	return nil, false
 }
